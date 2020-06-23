10d08f811b7fec8d778022b0ef387a1a07de18b5
ir1d <sirius.caffrey@gmail.com>
2020-06-23 21:59:44 +0800

    add tfv2 implementation of Conv layer in Discriminator; add Conv2DSN

--- v2/inpaint_ops.py
+++ v2/inpaint_ops.py
@@ -117,8 +117,107 @@ class GenDeconvLayer(tf.keras.layers.Layer):
         x = self.conv(x)
         return x
 
-@add_arg_scope
-def dis_conv(x, cnum, ksize=5, stride=2, name='conv', training=True):
+def l2_norm(input_x, epsilon=1e-12):
+    input_x_norm = input_x / (tf.reduce_sum(input_x**2)**0.5 + epsilon)
+    return input_x_norm
+
+def power_iteration(u, ite):
+    v_ = tf.matmul(u, tf.transpose(w_mat))
+    v_hat = l2_norm(v_)
+    u_ = tf.matmul(v_hat, w_mat)
+    u_hat = l2_norm(u_)
+    return u_hat, v_hat, ite+1
+
+class kernel_spectral_norm(tf.keras.layers.Layer):
+    def __init__(self, iteration=1, name='kernel_sn'):
+        self.iteration = iteration
+        self.name = name
+    def call(self, inputs):
+        # inputs is kernel
+        w_shape = inputs.get_shape().as_list()
+        w_mat = tf.reshape(inputs, [-1, w_shape[-1]])
+        u = tf.Variable(name='u', shape=[1, w_shape[-1]], initial_value=tf.random.truncated_normal(shape=[1, w_shape[-1]]), trainable=False)
+
+        u_hat, v_hat,_ = power_iteration(u, self.iteration)
+        sigma = tf.matmul(tf.matmul(v_hat, w_mat), tf.transpose(u_hat))
+        w_mat = w_mat / sigma
+        with tf.control_dependencies([u.assign(u_hat)]):
+            w_norm = tf.reshape(w_mat, w_shape)
+        return w_norm
+    
+
+class Conv2DSepctralNorm(tf.layers.Conv2D):
+    def build(self, input_shape):
+        super(Conv2DSepctralNorm, self).build(input_shape)
+        self.sn = kernel_spectral_norm()
+        self.kernel = self.sn(self.kernel)
+
+class conv2d_spectral_norm(tf.keras.layers.Layer):
+    """
+    https://github.com/JiahuiYu/neuralgym/blob/88292adb524186693a32404c0cfdc790426ea441/neuralgym/ops/gan_ops.py#L144
+    """
+    def __init__(filters,
+        kernel_size,
+        strides=(1, 1),
+        padding='valid',
+        data_format='channels_last',
+        dilation_rate=(1, 1),
+        activation=None,
+        use_bias=True,
+        kernel_initializer=None,
+        bias_initializer=tf.zeros_initializer(),
+        kernel_regularizer=None,
+        bias_regularizer=None,
+        activity_regularizer=None,
+        kernel_constraint=None,
+        bias_constraint=None,
+        trainable=True,
+        name=None,
+        reuse=None):
+        super(conv2d_spectral_norm, self).__init__()
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.padding = padding
+        self.data_format = data_format
+        self.dilation_rate = dilation_rate
+        self.activation = activation
+        self.use_bias = use_bias
+        self.kernel_initializer = kernel_initializer
+        self.bias_initializer = bias_initializer
+        self.kernel_regularizer = kernel_regularizer
+        self.bias_regularizer = bias_regularizer
+        self.activity_regularizer = activity_regularizer
+        self.kernel_constraint = kernel_constraint
+        self.bias_constraint = bias_constraint
+        self.trainable = trainable
+        self.name = name
+
+        self.layer = Conv2DSepctralNorm(
+        filters=filters,
+        kernel_size=kernel_size,
+        strides=strides,
+        padding=padding,
+        data_format=data_format,
+        dilation_rate=dilation_rate,
+        activation=activation,
+        use_bias=use_bias,
+        kernel_initializer=kernel_initializer,
+        bias_initializer=bias_initializer,
+        kernel_regularizer=kernel_regularizer,
+        bias_regularizer=bias_regularizer,
+        activity_regularizer=activity_regularizer,
+        kernel_constraint=kernel_constraint,
+        bias_constraint=bias_constraint,
+        trainable=trainable,
+        name=name)
+
+    # def build()
+    def call(self, inputs, training=True):
+        return self.layer.apply(inputs)
+
+
+class DisConvLayer(tf.keras.layers.Layer):
     """Define conv for discriminator.
     Activation is set to leaky_relu.
 
@@ -134,9 +233,18 @@ def dis_conv(x, cnum, ksize=5, stride=2, name='conv', training=True):
         tf.Tensor: output
 
     """
-    x = conv2d_spectral_norm(x, cnum, ksize, stride, 'SAME', name=name)
-    x = tf.nn.leaky_relu(x)
-    return x
+    def __init__(self, cnum, ksize=5, stride=2, name='conv', training=True):
+        super(DisConvLayer, self).__init__()
+        self.cnum = cnum
+        self.ksize = ksize
+        self.stride = stride
+        self.name = name
+        self.training = training
+        self.conv = conv2d_spectral_norm(x, self.cnum, self.ksize, self.stride, 'SAME', name=self.name)
+    def call(self, inputs, training=True):
+        x = self.conv(inputs)
+        x = tf.nn.leaky_relu(x)
+        return x
 
 
 def random_bbox(FLAGS):
