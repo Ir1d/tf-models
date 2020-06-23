5e122d9494cd06da99aca1e61545cd715f393ac2
ir1d <sirius.caffrey@gmail.com>
2020-06-23 21:24:39 +0800

    add tfv2 implementation of DeConv layer in Generator also include implementation of resize ops from neuralgym.ops.layers

--- v2/inpaint_ops.py
+++ v2/inpaint_ops.py
@@ -67,9 +67,29 @@ class GenConvLayer(tf.keras.layers.Layer):
         return x
 
 
-@add_arg_scope
-def gen_deconv(x, cnum, name='upsample', padding='SAME', training=True):
-    """Define deconv for generator.
+
+def resize(x, scale=2, to_shape=None, align_corners=True, dynamic=False,
+           func=tf.image.resize_bilinear, name='resize'):
+    """Resize a given image.
+    Originated from https://github.com/JiahuiYu/neuralgym/blob/88292adb524186693a32404c0cfdc790426ea441/neuralgym/ops/layers.py#L141
+    """
+    if dynamic:
+        xs = tf.cast(tf.shape(x), tf.float32)
+        new_xs = [tf.cast(xs[1]*scale, tf.int32),
+                  tf.cast(xs[2]*scale, tf.int32)]
+    else:
+        xs = x.get_shape().as_list()
+        new_xs = [int(xs[1]*scale), int(xs[2]*scale)]
+    with tf.variable_scope(name):
+        if to_shape is None:
+            x = func(x, new_xs, align_corners=align_corners)
+        else:
+            x = func(x, [to_shape[0], to_shape[1]],
+                     align_corners=align_corners)
+    return x
+
+class GenDeconvLayer(tf.keras.layers.Layer):
+     """Define deconv for generator.
     The deconv is defined to be a x2 resize_nearest_neighbor operation with
     additional gen_conv operation.
 
@@ -83,15 +103,19 @@ def gen_deconv(x, cnum, name='upsample', padding='SAME', training=True):
         tf.Tensor: output
 
     """
-    with tf.compat.v1.variable_scope(name):
-        x = resize(x, func=tf.compat.v1.image.resize_nearest_neighbor)
-        x = gen_conv(
-            x, cnum, 3, 1, name=name+'_conv', padding=padding,
-            training=training)
-    return x
-
-
-
+    def __init__(self, cnum, name='upsample', padding='SAME', training=True):
+        super(GenDeconvLayer, self).__init__()
+        self.cnum = cnum
+        self.name = name
+        self.padding = padding
+        self.training = training
+        self.conv = GenConvLayer(cnum=cnum, ksize=3, stride=1, name=self.name + '_conv', padding=self.padding, training=self.training)
+    def call(self, inputs, training=True):
+        xs = inputs.get_shape().as_list()
+        new_xs = [int(xs[1] * 2), int(xs[2] *2)]
+        x = tf.image.resize(images=inputs, size=new_xs, method=ResizeMethod.NEAREST_NEIGHBOR, align_corners=True)
+        x = self.conv(x)
+        return x
 
 @add_arg_scope
 def dis_conv(x, cnum, ksize=5, stride=2, name='conv', training=True):
